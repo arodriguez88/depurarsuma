179b71c15eb4f085608937e1f8b30c88fdb21534
vesprada <vesprada@localhost>
2014-12-18 20:45:41 +0100

    commit1

---
+++ src/depurarsuma/Depurarsuma.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package depurarsuma;
+
+/**
+ *
+ * @author vesprada
+ */
+public class Depurarsuma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numMax = 100;
+        int numActual = 1;
+        int incremento = 1;
+        while (numActual <= numMax) {
+        System.out.print(numActual);
+        numActual +=incremento;
+        incremento++;
+        }
+        
+    }
+    
+}
